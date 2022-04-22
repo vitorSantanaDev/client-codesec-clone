9d96186b9585aac4614b38def5dc3cbe048f9746
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-04-22 19:39:38 -0300

    ci: :wrench: setting theme and background in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,23 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
+
+import { theme } from '../src/styles/theme/theme'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 
 export const parameters = {
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white
+      },
+      {
+        name: 'dark',
+        value: theme.colors.blackDark
+      }
+    ]
+  },
   nextRouter: {
     Provider: RouterContext.Provider
   }
@@ -9,9 +25,9 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
