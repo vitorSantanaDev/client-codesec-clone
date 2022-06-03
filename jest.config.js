446802eba6b99fed49f7e24c6b50537e657b63fe
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-02 17:56:17 -0700

    ci: :wrench: some changes in settings files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,21 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.tsx',
+    '!<rootDir>/src/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/styles/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/pages/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/components/Loading/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/components/SectionTestimonials/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/graphql/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/graphql/queries/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/routes/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/*.stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
