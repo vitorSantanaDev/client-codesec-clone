f686e0073fec37fba6e28fdcad7b824e649cae38
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-06 18:53:09 -0700

    ci: :test_tube: improving test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,15 +5,17 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/**/*.stories.tsx',
+    '!src/**/*.types.tsx',
+    '!src/interfaces/*.types.tsx',
     '!<rootDir>/src/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/styles/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/pages/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/components/Loading/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/components/SectionTestimonials/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/graphql/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/graphql/queries/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/routes/*.{js,jsx,ts,tsx}',
     '!<rootDir>/**/*.stories.{js,jsx,ts,tsx}',
     '!<rootDir>/node_modules/'
   ],
